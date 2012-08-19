b3d09982df685ca8d6267d353dfd8ffa67ddb580
Chris Frantz <cfrantz@bunk.(none)>
2012-08-19 15:42:51 -0500

    Add cmpxchg32 and volatile

--- include/pongo/atomic.h
+++ include/pongo/atomic.h
@@ -2,7 +2,16 @@
 #define PONGO_ATOMIC_H
 #include <pongo/stdtypes.h>
 
-static inline int cmpxchg(uint64_t *ptr, uint64_t oldval, uint64_t newval)
+static inline int cmpxchg32(volatile uint32_t *ptr, uint32_t oldval, uint32_t newval)
+{
+#ifdef WIN32
+    return _InterlockedCompareExchange32(ptr, newval, oldval) == oldval;
+#else
+    return __sync_bool_compare_and_swap(ptr, oldval, newval);
+#endif
+}
+
+static inline int cmpxchg64(volatile uint64_t *ptr, uint64_t oldval, uint64_t newval)
 {
 #ifdef WIN32
     return _InterlockedCompareExchange64(ptr, newval, oldval) == oldval;
@@ -12,25 +21,25 @@ static inline int cmpxchg(uint64_t *ptr, uint64_t oldval, uint64_t newval)
 }
 
 static inline uint32_t
-atomic_add(uint32_t *a, uint32_t addval)
+atomic_add(volatile uint32_t *a, uint32_t addval)
 {
     return __sync_add_and_fetch(a, addval);
 }
 
 static inline uint32_t
-atomic_subtract(uint32_t *a, uint32_t subval)
+atomic_subtract(volatile uint32_t *a, uint32_t subval)
 {
     return __sync_sub_and_fetch(a, subval);
 }
 
 static inline uint32_t
-atomic_inc(uint32_t *a)
+atomic_inc(volatile uint32_t *a)
 {
     return atomic_add(a, 1);
 }
 
 static inline uint32_t
-atomic_dec(uint32_t *a)
+atomic_dec(volatile uint32_t *a)
 {
     return atomic_subtract(a, 1);
 }
